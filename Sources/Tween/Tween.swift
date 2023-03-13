de745a001642b0e700d4ff1b0189bd8722346112
yangfengzzz <yangfengzzz@hotmail.com>
2023-03-13 20:20:49 +0800

    link math

--- Sources/Tween/Tween.swift
+++ Sources/Tween/Tween.swift
@@ -1,6 +1,23 @@
-public struct Tween {
-    public private(set) var text = "Hello, World!"
+import Math
 
-    public init() {
+/// Struct that stores two colors (used for LineRenderer tweens)
+public struct Color2 {
+    public var ca: Color, cb: Color
+
+    public init(ca: Color, cb: Color) {
+        self.ca = ca;
+        self.cb = cb;
+    }
+
+    public static func +(c1: Color2, c2: Color2) -> Color2 {
+        Color2(ca: c1.ca + c2.ca, cb: c1.cb + c2.cb);
+    }
+
+    public static func -(c1: Color2, c2: Color2) -> Color2 {
+        Color2(ca: c1.ca - c2.ca, cb: c1.cb - c2.cb);
+    }
+
+    public static func *(c1: Color2, f: Float) -> Color2 {
+        Color2(ca: c1.ca * f, cb: c1.cb * f);
     }
 }
